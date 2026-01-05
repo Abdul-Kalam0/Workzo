16089c886c20dc79a048901b78f9ee3e68535c26
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-01-06 00:05:58 +0530

    Fixed the cors and token issue

--- server/controllers/authControlles.js
+++ server/controllers/authControlles.js
@@ -159,6 +159,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // ✅
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✅
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
